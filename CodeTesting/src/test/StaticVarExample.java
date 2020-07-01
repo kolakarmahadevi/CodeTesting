76e6db60c73863d9128435ccc9d14f718b7e48c5
mahadevi <kolkarmahadevi@gmail.com>
2020-07-01 11:28:42 +0530

    Code test project

---
+++ CodeTesting/src/test/StaticVarExample.java
@@ -0,0 +1,14 @@
+package test;
+
+public class StaticVarExample {
+
+	public static void main(String[] args) {
+		int i=10;
+        while(i>1){
+             System.out.println(i);
+             i--;
+        }
+
+	}
+
+}
